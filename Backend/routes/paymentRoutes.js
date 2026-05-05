51250c81083af48293a9a65bf38424bafa04df24
Jiya Chaurasia <jiyachaurasia25@gmail.com>
2026-05-05 21:32:07 +0530

    Update paymentRoutes.js

--- Backend/routes/paymentRoutes.js
+++ Backend/routes/paymentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 const fetchUser = require('../middleware/fetchUser');
 
-router.post('/create-order',  paymentController.createOrder);
+router.post('/create-order', fetchUser,  paymentController.createOrder);
 router.post('/verify-payment', fetchUser, paymentController.verifyPayment);
 router.get('/my-orders', fetchUser, paymentController.getUserOrders);
 router.get('/get-key', paymentController.getRazorpayKey);
