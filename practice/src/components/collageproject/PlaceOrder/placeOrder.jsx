06c449c127e3afb9504cc6e3ff7ad2b0d723c265
Jiya Chaurasia <jiyachaurasia25@gmail.com>
2026-05-10 10:34:24 +0530

    Update placeOrder.jsx

--- practice/src/components/collageproject/PlaceOrder/placeOrder.jsx
+++ practice/src/components/collageproject/PlaceOrder/placeOrder.jsx
@@ -89,7 +89,7 @@ function PlaceOrder() {
       }
 
       // 1. Create order on backend
-      const { data } = await axios.post("http://localhost:4000/api/payment/create-order", {
+      const { data } = await axios.post("https://shopper-backend-37ge.onrender.com/api/payment/create-order", {
         amount: orderTotal,
         items,
         address: form
@@ -98,7 +98,7 @@ function PlaceOrder() {
       });
 
       // Fetch Razorpay Key
-      const { data: { key } } = await axios.get("http://localhost:4000/api/payment/get-key");
+      const { data: { key } } = await axios.get("https://shopper-backend-37ge.onrender.com/api/payment/get-key");
 
       if (data.success) {
         // 2. Open Razorpay Popup
@@ -113,7 +113,7 @@ function PlaceOrder() {
           handler: async function (response) {
             // 3. Verify Payment
             try {
-              const verifyRes = await axios.post("http://localhost:4000/api/payment/verify-payment", {
+              const verifyRes = await axios.post("https://shopper-backend-37ge.onrender.com/api/payment/verify-payment", {
                 razorpay_order_id: response.razorpay_order_id,
                 razorpay_payment_id: response.razorpay_payment_id,
                 razorpay_signature: response.razorpay_signature,
